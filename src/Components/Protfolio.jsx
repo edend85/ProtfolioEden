3745e0535825bc1b873fc68df0dd8f796bcb6bce
edend85 <117260021+edend85@users.noreply.github.com>
2024-02-29 14:21:54 +0200

    adding array to json + css

--- src/Components/Protfolio.jsx
+++ src/Components/Protfolio.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import '../styles/Styels.css';
 import PrjIcon from '../assets/Images/project.png';
+import PrjList from '../Data/Projects.json';
 
-export default function Protfolio() {
-    return (
-        <div id='main-projects'>
-            <div className='projects-title'>
-                <h2>Protfolio</h2>
-            </div>
-            <div id='projects-grid'>
-                <div className='singal-project'>
+/* 
+<div className='singal-project'>
                     <div className='singal-project-columns'>
                         <img src={PrjIcon} />
                         <h4>Final Project - SmokeEye</h4>
                         <div className='singal-project-btns'>
-                            <button>Demo</button>
-                            <button>GitHub</button>
+                            <button><a href=""></a>Demo</button>
+                            <button><a href=""></a>GitHub</button>
                         </div>
                     </div>
                     <p className='bold'>Note: This project is done with a partner</p>
@@ -33,17 +28,45 @@ export default function Protfolio() {
                         <img src={PrjIcon} />
                         <h4>Authentication Website</h4>
                         <div className='singal-project-btns'>
-                            <button>Demo</button>
-                            <button>GitHub</button>
+                            <button><a href="https://authentication-zi9e.onrender.com"></a>Demo</button>
+                            <button><a href=""></a>GitHub</button>
                         </div>
                     </div>
                     <p>secure authentication website that allows users to sign up, log in
                         using email and password, and utilize social media login options
                         with Google and Facebook. The project uses a stack of
                     </p>
-                    <p className='gray-p'>technologies
-                        including: <br></br> HTML, CSS, Node.js, JavaScript (React.js, React Native), MongoDB.</p>
+                    <p className='gray-p'>technologies including: <br></br> HTML, CSS, Node.js, JavaScript (React.js, React Native), MongoDB.</p>
                 </div>
+*/
+
+export default function Protfolio() {
+    return (
+        <div id='main-projects'>
+            <div className='projects-title'>
+                <h2>Protfolio</h2>
+            </div>
+            <div id='projects-grid'>
+                {
+                    PrjList.map((Prj, index) => {
+                        return (
+                            <div className='singal-project' key={index}>
+                                <div className='singal-project-columns'>
+                                    <img src={PrjIcon} />
+                                    <h4>{Prj.prjtitle}</h4>
+                                    <div className='singal-project-btns'>
+                                        <button><a href={Prj.prjdemolink}></a>Demo</button>
+                                        <button><a href={Prj.prjgithublink}></a>GitHub</button>
+                                    </div>
+                                </div>
+                                <p className='bold'>{Prj.note}</p>
+                                <p>{Prj.prjdescription}</p>
+                                <p className='gray-p'>technologies including: <br></br>{Prj.prjtechnologies}</p>
+                            </div>
+                        )
+
+                    })
+                }
             </div>
         </div>
     )
