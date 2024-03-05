a715755774dad80c883aaf9ed8bf36e4de9cc4cf
edend85 <117260021+edend85@users.noreply.github.com>
2024-03-05 15:03:00 +0200

    Update Protfolio.jsx

--- src/Components/Protfolio.jsx
+++ src/Components/Protfolio.jsx
@@ -20,7 +20,7 @@ export default function Protfolio() {
                                     <h4>{Prj.prjtitle}</h4>
                                     <div className='singal-project-btns'>
                                         {
-                                            Prj.prjdemolink == null ? <a href={Prj.prjdemolink} aria-disabled="true">Demo</a> : <a href={Prj.prjdemolink}>Demo</a>
+                                            Prj.prjdemolink == null ? <a href={Prj.prjdemolink} style={{ pointerEvents: "none" }}>Demo</a> : <a href={Prj.prjdemolink}>Demo</a>
                                         }
                                         <a href={Prj.prjgithublink}>GitHub</a>
                                     </div>
